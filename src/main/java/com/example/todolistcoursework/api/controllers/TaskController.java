2125254eb0e1a58d715ece465cdf9ef93bd15a40
kaori <pestrikv1337@gmail.com>
2023-01-09 15:40:54 +0600

    Second commit

--- src/main/java/com/example/todolistcoursework/api/controllers/TaskController.java
+++ src/main/java/com/example/todolistcoursework/api/controllers/TaskController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping("")
-//g
+//gg
 @RestController
 @RequiredArgsConstructor
 public class TaskController {
