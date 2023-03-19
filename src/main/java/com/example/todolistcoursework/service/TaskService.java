64046c36692f3a211025e92eb7c82af00db4b6a3
kaori <pestrikv1337@gmail.com>
2023-03-19 20:02:49 +0300

    task mapper,services instant fix

--- src/main/java/com/example/todolistcoursework/service/TaskService.java
+++ src/main/java/com/example/todolistcoursework/service/TaskService.java
@@ -38,6 +38,7 @@ public class TaskService {
     public Task createTask(Task task) {
         User user = getUser();
         task.setUser(user);
+        task.setCreated(task.getCreated());
         taskRepository.save(task);
         return task;
     }
--- src/main/java/com/example/todolistcoursework/util/TaskMapper.java
+++ src/main/java/com/example/todolistcoursework/util/TaskMapper.java
@@ -5,12 +5,15 @@ import com.example.todolistcoursework.model.entity.Task;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 @RequiredArgsConstructor
 public class TaskMapper {
     public Task toTask(TaskDto taskDto) {
         Task task = new Task();
         task.setData(taskDto.getData());
+        task.setCreated(LocalDateTime.now());
         task.setDeadline(null);
         task.setModified(null);
         task.setCheckbox(taskDto.isCheckbox());
