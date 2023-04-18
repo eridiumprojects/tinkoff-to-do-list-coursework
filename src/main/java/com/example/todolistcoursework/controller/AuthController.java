50daa219abdfc6fc902763e8b14925e507136a10
pestrikv <pestrikv1337@gmail.com>
2023-04-18 21:37:37 +0300

    rebase master

--- src/main/java/com/example/todolistcoursework/controller/AuthController.java
+++ src/main/java/com/example/todolistcoursework/controller/AuthController.java
@@ -3,14 +3,18 @@ package com.example.todolistcoursework.controller;
 import com.example.todolistcoursework.model.dto.request.LoginRequest;
 import com.example.todolistcoursework.model.dto.request.RefreshRequest;
 import com.example.todolistcoursework.model.dto.request.SignupRequest;
-import com.example.todolistcoursework.model.dto.response.UserInfoResponse;
 import com.example.todolistcoursework.service.AuthService;
 import com.example.todolistcoursework.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -20,18 +24,30 @@ public class AuthController {
     private final UserService userService;
     private final AuthService authService;
 
+    @Operation(summary = "Refresh the access token")
+    @ApiResponse(responseCode = "200", description = "Access token refreshed successfully")
     @PostMapping("/refresh")
     public ResponseEntity<?> refreshToken(@Valid @RequestBody RefreshRequest request) {
         return ResponseEntity.ok(authService.refresh(request));
     }
 
+    @Operation(summary = "Authenticate user and return access token")
+    @ApiResponse(responseCode = "200", description = "User authenticated successfully")
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         return ResponseEntity.ok(userService.loginUser(loginRequest));
     }
 
+    @Operation(summary = "Register a new user and return access token")
+    @ApiResponse(responseCode = "200", description = "User registered successfully")
     @PostMapping("/signup")
-    public ResponseEntity<UserInfoResponse> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
         return ResponseEntity.ok(userService.registerUser(signUpRequest));
     }
 }
+
+
+
+
+
+
