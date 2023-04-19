35534655b5d65b5fa14063149f276df35a8ed9cd
strelnikov <for.back.school@yandex.ru>
2023-04-19 22:32:01 +0300

    Fix swagger

--- src/main/java/com/example/todolistcoursework/controller/UserController.java
+++ src/main/java/com/example/todolistcoursework/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.example.todolistcoursework.controller;
 
 import com.example.todolistcoursework.model.dto.response.UserInfoResponse;
-import com.example.todolistcoursework.model.entity.User;
 import com.example.todolistcoursework.model.exception.AuthException;
-import com.example.todolistcoursework.model.exception.ObjectAlreadyExists;
 import com.example.todolistcoursework.service.AuthService;
 import com.example.todolistcoursework.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -24,30 +21,31 @@ public class UserController {
     private final UserService userService;
 
     @Operation(summary = "Get current user information")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "User information received successfully"
-                    , content = @Content(
-                    array = @ArraySchema(
-                            schema = @Schema(implementation = UserInfoResponse.class)
-                    )
-            )),
-            @ApiResponse(responseCode = "409", description = "Username doesn't exists"
-                    , content = @Content(
-                    array = @ArraySchema(
-                            schema = @Schema(implementation = AuthException.class)
-                    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User information received successfully",
+                    content = @Content(schema = @Schema(implementation = UserInfoResponse.class))
+                    ),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Username doesn't exists",
+                    content = @Content(schema = @Schema(implementation = AuthException.class))
             )
-            )})
+    })
     @GetMapping("/current")
     public ResponseEntity<UserInfoResponse> getCurrentUser() {
         return ResponseEntity.ok(userService.getUserInfo(authService.getJwtAuth().getUserId()));
     }
 
-    @Operation(summary = "Delete current user", responses = {
-            @ApiResponse(responseCode = "200",
+    @Operation(summary = "Delete current user")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
                     description = "User deleted successfully"),
-            @ApiResponse(responseCode = "409",
-            description = "User doesn't exists")
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "User doesn't exists")
     })
     @DeleteMapping("/current")
     public ResponseEntity<UserInfoResponse> deleteCurrentUser() {
