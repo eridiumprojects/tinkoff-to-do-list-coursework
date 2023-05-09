2a62f444f0c0e374b0e692e8d2afdeec5cb016a5
pestrikv <pestrikv1337@gmail.com>
2023-05-09 19:07:28 +0300

    Added taskServiceTest

---
+++ src/test/java/com/example/todolistcoursework/service/TaskServiceTest.java
@@ -0,0 +1,141 @@
+package com.example.todolistcoursework.service;
+
+import com.example.todolistcoursework.builder.TaskMapper;
+import com.example.todolistcoursework.model.dto.response.TaskInfo;
+import com.example.todolistcoursework.model.entity.Task;
+import com.example.todolistcoursework.model.entity.User;
+import com.example.todolistcoursework.repository.TaskRepository;
+import com.example.todolistcoursework.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@WithMockUser(username = "anton", roles = "USER")
+class TaskServiceTest {
+    @Mock
+    public TaskRepository taskRepository;
+    @Mock
+    public UserRepository userRepository;
+    @Mock
+    public PageRequest pageRequest;
+    @InjectMocks
+    public TaskService taskService;
+
+    public Task createTaskByDefault() {
+        return Task.builder()
+                .id(1L)
+                .description("Join in Tinkoff")
+                .data("Pass an interview")
+                .user(createUserByDefault())
+                .build();
+    }
+
+    public User createUserByDefault() {
+        return User.builder()
+                .id(1L)
+                .firstName("Anton")
+                .lastName("Pestrikov")
+                .username("eridium")
+                .email("pestrikov@mail.ru")
+                .password("pestrikov123")
+                .devices(List.of())
+                .build();
+    }
+
+    public TaskInfo createTaskInfoByDefault() {
+        return TaskMapper.toApi(createTaskByDefault());
+    }
+
+    public List<Task> createListOfTasksByDefault() {
+        return List.of(
+                Task.builder()
+                        .id(1L)
+                        .description("Join in Tinkoff")
+                        .data("Pass an interview")
+                        .user(createUserByDefault())
+                        .build(),
+                Task.builder()
+                        .id(2L)
+                        .description("Breakfast")
+                        .data("Take a meal")
+                        .user(createUserByDefault())
+                        .build()
+        );
+    }
+
+    public List<TaskInfo> createListOfTasksInfoByDefault() {
+        return createListOfTasksByDefault()
+                .stream()
+                .map(TaskMapper::toApi)
+                .collect(Collectors.toList());
+    }
+
+    @Test
+    void createTaskSuccess() {
+        //given
+        User user = createUserByDefault();
+        Task actualTask = createTaskByDefault();
+        TaskInfo expectedTask = createTaskInfoByDefault();
+        //when
+        when(taskRepository.save(any(Task.class))).thenReturn(actualTask);
+        when(userRepository.findById(anyLong())).thenReturn(Optional.ofNullable(user));
+        TaskInfo afterResponse = taskService.createTask(user.getId(), actualTask);
+        //then
+        assertNotNull(afterResponse);
+        assertEquals(expectedTask,afterResponse);
+    }
+
+    @Test
+    void getTaskSuccess() {
+        //given
+        Task actualTask = createTaskByDefault();
+        TaskInfo expectedTask = createTaskInfoByDefault();
+        //when
+        when(taskRepository.findById(anyLong())).thenReturn(Optional.ofNullable(actualTask));
+        TaskInfo afterResponse = taskService.getTask(1L, 1L);
+        //then
+        assertNotNull(afterResponse);
+        assertEquals(expectedTask, afterResponse);
+    }
+
+    @Test
+    void updateTaskSuccess() {
+        //given
+        Task actualTask = createTaskByDefault();
+        Task updatedTask = createTaskByDefault();
+        updatedTask.setData("new data");
+        TaskInfo oldInfo = createTaskInfoByDefault();
+        //when
+        when(taskRepository.findById(anyLong())).thenReturn(Optional.of(actualTask));
+        when(taskRepository.save(any(Task.class))).thenReturn(updatedTask);
+        TaskInfo newInfo = taskService.updateTask(1L, actualTask);
+        //then
+        assertNotEquals(oldInfo,newInfo);
+        assertNotNull(newInfo);
+    }
+
+    @Test
+    void deleteTaskSuccess() {
+        //given
+        Task actualTask = createTaskByDefault();
+        //when
+        when(taskRepository.findById(anyLong())).thenReturn(Optional.ofNullable(actualTask));
+
+        //then
+    }
+
+}
--- test/to-do-list-coursework/controller.init.script/embed.sql
+++ test/to-do-list-coursework/controller.init.script/embed.sql
@@ -12,12 +12,12 @@ VALUES (1,2),
        (2,1);
 
 INSERT INTO task_test(id, created, data, deadline, modified, user_id, description, status)
-VALUES (1, '2023-05-07 14:42:16.553606', 'New', '2023-07-07 14:42:16.553606', '2023-05-07 15:42:16.553606', 1, 'description', 'BACKLOG')
+VALUES (1, '2023-05-07 14:42:16.553606', 'New', '2023-07-07 14:42:16.553606', '2023-05-07 15:42:16.553606', 1, 'description', 'BACKLOG');
 
 
 INSERT INTO device_test(id, created, device_token, last_login, user_id)
 VALUES (1, '2023-05-03 14:42:16.553606', 'f45b2dac-ee52-11ed-a05b-0242ac120003', '2023-07-03 14:42:16.553606', 1),
-       (2, '2023-06-03 14:42:16.553606', 'a700584d-8485-40dd-96b8-483b2c4d8601', '2023-08-03 14:42:16.553606', 2);
+    (2, '2023-06-03 14:42:16.553606', 'a700584d-8485-40dd-96b8-483b2c4d8601', '2023-08-03 14:42:16.553606', 2);
 
 INSERT INTO refresh_token_test(id, token, device_id, user_id)
 VALUES (1, '3b68d4f6-ee53-11ed-a05b-0242ac120003',1,1),
